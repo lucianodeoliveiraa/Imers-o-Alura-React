2bd54ddfc68fee24433c25387aa9059b421cc1e6
Luciano de Oliveira <lucianooliveira825@gmail.com>
2020-07-28 23:45:00 -0300

    Primeiro Commit

---
+++ src/components/Menu/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+import Button from '../../components/Button';
+
+function Menu(){
+    return(
+        <nav className="Menu">
+            <Link to="/">
+              <img className="Logo" src={Logo} alt="Lucianoflix logo"/>
+            </Link>
+
+            <Button as= {Link} className="ButtonLink"to="/cadastro/video">
+             Novo video
+            </Button>
+
+        </nav>
+    );
+}
+
+
+export default Menu;
